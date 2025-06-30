4debc6543a77df42b32fc88075d82371c1cc9ea7
scott <scott.allo@laplateforme.io>
2025-06-30 14:46:34 +0200

    Ajout de la sidebar de navigation et intégration au layout principal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,16 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import BookForm from './pages/BookForm';
+import Sidebar from './components/Sidebar/Sidebar';
+
+const Layout = ({ children }) => (
+  <div style={{ display: 'flex' }}>
+    <Sidebar />
+    <div style={{ marginLeft: 200, width: '100%' }}>
+      {children}
+    </div>
+  </div>
+);
 
 const App = () => {
   return (
@@ -11,9 +21,9 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/add-book" element={<BookForm />} />
-        {/* D'autres routes à venir */}
+        <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
+        <Route path="/add-book" element={<Layout><BookForm /></Layout>} />
+        {/* D'autres routes à venir, protégées par Layout */}
       </Routes>
     </Router>
   );
